419f1a3624fb3ee0b5ce84227f0a721ec388b442
AvasBrahma <avasbrahma@gmail.com>
2023-07-10 21:07:57 +0530

    home Ipod UI implemented

---
+++ src/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomeIpod = (props) => {
+    return (
+		<div className="ipod">
+           <div className="top-container">
+
+           </div>
+        </div>
+    )
+}
+
+export default HomeIpod;
